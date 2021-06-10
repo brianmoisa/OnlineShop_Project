986e0c09ac1fe4567962341f1db1e3a412516bde
Brian Geordy <brianmoisa@gmail.com>
2021-06-10 16:16:08 +0300

    test from 2

--- Online Shop/OnlineShop/OnlineShop/Program.cs
+++ Online Shop/OnlineShop/OnlineShop/Program.cs
@@ -21,7 +21,7 @@ namespace OnlineShop
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
         
-//TESTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
+//ablabla
 
     }
 }
